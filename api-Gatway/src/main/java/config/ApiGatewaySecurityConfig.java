520c70a98df2bd93fcc89f1fb3ed3f7cd955a464
arbi ferchichi <arbi.ferchichi@esprit.tn>
2024-11-12 16:49:37 +0100

    Initial commit with all folders

---
+++ api-Gatway/src/main/java/config/ApiGatewaySecurityConfig.java
@@ -0,0 +1,44 @@
+package config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+
+
+@EnableWebFluxSecurity
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+
+public class ApiGatewaySecurityConfig {
+
+
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity
+                                                                 serverHttpSecurity) {
+        return serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange().authenticated()
+                ).oauth2ResourceServer((oauth) -> oauth
+                        .jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+
+
+}
+
